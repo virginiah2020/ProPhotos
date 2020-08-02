6e274b6d12f9e708871089507c6da0720816efb7
virginiah2020 <vancyvy254@gmail.com>
2020-08-02 10:53:26 +0300

    imorted statics

--- images/urls.py
+++ images/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url,include
-from . import viewsfrom django.conf import settings
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 
