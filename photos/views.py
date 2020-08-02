6396b901535ec1ab4129565341ed8575f265574b
virginiah2020 <vancyvy254@gmail.com>
2020-08-02 07:25:36 +0300

    def function to view photos

--- photos/views.py
+++ photos/views.py
@@ -2,5 +2,10 @@ from django.http  import HttpResponse
 import datetime as dt
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+def photos(request):
+
+    images=Image.objects.all()
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+
+    return render(request,'images.html',{"images":images,"ALLOWED_HOSTS":ALLOWED_HOSTS})
+
