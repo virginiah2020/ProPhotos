a1724ec563489cd6b4012a024ee4f701706dbb5c
virginiah2020 <vancyvy254@gmail.com>
2020-08-02 11:48:30 +0300

    installed psycopg2

--- images/views.py
+++ images/views.py
@@ -3,12 +3,12 @@ from django.http  import HttpResponse
 import datetime as dt
 
 # Create your views here.
-def photos(request):
+def images(request):
 
-    photos=Photo.objects.all()
+    images=Image.objects.all()
     ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
-    return render(request,'photos.html',{"photos":images,"ALLOWED_HOSTS":ALLOWED_HOSTS})
+    return render(request,'photos.html',{"images":images,"ALLOWED_HOSTS":ALLOWED_HOSTS})
 
 # def search_results(request):
 #     if 'photos' in request.GET and request.GET["photos"]:
--- photosapp/settings.py
+++ photosapp/settings.py
@@ -14,6 +14,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIR=[
+    os.path.join(BASE_DIR,'static')
+]
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photos',
+    'images',
     'bootstrap3',
 ]
 
@@ -119,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/img/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"img")
 ]
