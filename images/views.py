62fd8e78903cf4269c2ffaffd76616b0e33775d9
virginiah2020 <vancyvy254@gmail.com>
2020-08-02 12:18:51 +0300

    impoted image on views

--- images/views.py
+++ images/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
+from .models import Image
+
 
 # Create your views here.
 def images(request):
