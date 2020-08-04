21b7b74b444182ba23d81e77bc34a531d6cd3d33
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-04 11:00:38 +0300

    added allowed host

--- photosapp/settings.py
+++ photosapp/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thawing-sierra-21400.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
